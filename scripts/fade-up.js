a56d90c29fb9c29bfa49f60395a5c4784dfb4334
ethanwads <eswadswo@ucsd.edu>
2020-10-14 23:37:57 -0700

    finished page content, starting page styling

--- scripts/fade-up.js
+++ scripts/fade-up.js
@@ -1,3 +1,23 @@
 $(document).ready(() => {
     $(".fade-up").fadeIn(2000)
 })
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar-custom").style.top = "0";
+  } else {
+    document.querySelector(".navbar-custom").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+let projectImg = document.querySelector('.project-img')
+projectImg.addEventListener('mouseenter', () => {
+    document.querySelector('.img-on-hover').style.display = "inline-block"
+})
+
+projectImg.addEventListener('mouseleave', () => {
+    document.querySelector('.img-on-hover').style.display = "none"
+})
