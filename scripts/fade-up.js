4397e825a74b72d4425a116f2093dd5e79cc8528
ethanwads <eswadswo@ucsd.edu>
2020-10-20 01:00:42 -0700

    small UI and file updates

--- scripts/fade-up.js
+++ scripts/fade-up.js
@@ -13,14 +13,5 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-let projectImg = document.querySelector('.project-img')
-projectImg.addEventListener('mouseenter', () => {
-    document.querySelector('.img-on-hover').style.display = "inline-block"
-})
-
-projectImg.addEventListener('mouseleave', () => {
-    document.querySelector('.img-on-hover').style.display = "none"
-})
-
 // use the intersection observer API for fade in up https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
 // https://www.selbekk.io/blog/2019/08/how-to-fade-in-content-as-it-scrolls-into-view/
